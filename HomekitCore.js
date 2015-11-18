a94967fd6266da097cf3d4f227f8af7847c418ae
Ralf Mayet <ralf@allthirds.com>
2015-11-18 19:16:58 +0100

    sockets initial implementation

--- HomekitCore.js
+++ HomekitCore.js
@@ -41,7 +41,7 @@ accessories.forEach(function(accessory) {
 
 // Publish the Bridge on the local network.
 bridge.publish({
-  username: "CC:22:3D:E3:CE:A9",
+  username: "CC:22:3D:E3:CE:A6",
   port: 51826,
   pincode: "031-45-154",
   category: Accessory.Categories.OTHER
