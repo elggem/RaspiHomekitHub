afd386c455a88b56b877fafd503cf2f0923f39d7
Ralf Mayet <ralf@tug-swl-228-224.tu-graz.ac.at>
2015-11-18 12:14:57 +0100

    added accessories

--- HomekitCore.js
+++ HomekitCore.js
@@ -12,7 +12,7 @@ console.log("HAP-NodeJS starting...");
 storage.initSync();
 
 // Start by creating our Bridge which will host all loaded Accessories
-var bridge = new Bridge('Node Bridge', uuid.generate("Node Bridge"));
+var bridge = new Bridge('Raspi Hub', uuid.generate("Raspi Hub"));
 
 // Listen for bridge identification event
 bridge.on('identify', function(paired, callback) {
--- accessories/ws2812_accessory.js
+++ accessories/ws2812_accessory.js
@@ -4,13 +4,15 @@ var Characteristic = require('../').Characteristic;
 var uuid = require('../').uuid;
 
 // here's a fake hardware device that we'll expose to HomeKit
-var FAKE_LIGHT = {
+var WS2812_LIGHT = {
   powerOn: false,
   brightness: 100, // percentage
   
   setPowerOn: function(on) { 
     console.log("Turning the light %s!", on ? "on" : "off");
     FAKE_LIGHT.powerOn = on;
+
+    //add callbacks to serial library here...
   },
   setBrightness: function(brightness) {
     console.log("Setting light brightness to %s", brightness);
@@ -29,30 +31,26 @@ var lightUUID = uuid.generate('hap-nodejs:accessories:light');
 // This is the Accessory that we'll return to HAP-NodeJS that represents our fake light.
 var light = exports.accessory = new Accessory('Light', lightUUID);
 
-// Add properties for publishing (in case we're using Core.js and not BridgedCore.js)
-light.username = "1A:2B:3C:4D:5E:FF";
-light.pincode = "031-45-154";
-
 // set some basic properties (these values are arbitrary and setting them is optional)
 light
   .getService(Service.AccessoryInformation)
-  .setCharacteristic(Characteristic.Manufacturer, "Oltica")
-  .setCharacteristic(Characteristic.Model, "Rev-1")
-  .setCharacteristic(Characteristic.SerialNumber, "A1S2NASF88EW");
+  .setCharacteristic(Characteristic.Manufacturer, "elggem")
+  .setCharacteristic(Characteristic.Model, "REV-0")
+  //.setCharacteristic(Characteristic.SerialNumber, "A1S2NASF88EW");
 
 // listen for the "identify" event for this Accessory
 light.on('identify', function(paired, callback) {
-  FAKE_LIGHT.identify();
+  WS2812_LIGHT.identify();
   callback(); // success
 });
 
 // Add the actual Lightbulb Service and listen for change events from iOS.
 // We can see the complete list of Services and Characteristics in `lib/gen/HomeKitTypes.js`
 light
-  .addService(Service.Lightbulb, "Fake Light") // services exposed to the user should have "names" like "Fake Light" for us
+  .addService(Service.Lightbulb, "Hub Light") // services exposed to the user should have "names" like "Fake Light" for us
   .getCharacteristic(Characteristic.On)
   .on('set', function(value, callback) {
-    FAKE_LIGHT.setPowerOn(value);
+    WS2812_LIGHT.setPowerOn(value);
     callback(); // Our fake Light is synchronous - this value has been successfully set
   });
 
@@ -69,7 +67,7 @@ light
     
     var err = null; // in case there were any problems
     
-    if (FAKE_LIGHT.powerOn) {
+    if (WS2812_LIGHT.powerOn) {
       console.log("Are we on? Yes.");
       callback(err, true);
     }
@@ -84,9 +82,9 @@ light
   .getService(Service.Lightbulb)
   .addCharacteristic(Characteristic.Brightness)
   .on('get', function(callback) {
-    callback(null, FAKE_LIGHT.brightness);
+    callback(null, WS2812_LIGHT.brightness);
   })
   .on('set', function(value, callback) {
-    FAKE_LIGHT.setBrightness(value);
+    WS2812_LIGHT.setBrightness(value);
     callback();
   })
