723ccbf0daefb25a34bc923f79473372f01f4505
Ralf Mayet <ralf@allthirds.com>
2015-11-21 23:38:52 +0100

    added motion accessory

---
+++ accessories/motion_sensor_accessory.js
@@ -0,0 +1,48 @@
+var Accessory = require('../').Accessory;
+var Service = require('../').Service;
+var Characteristic = require('../').Characteristic;
+var uuid = require('../').uuid;
+
+// here's a fake temperature sensor device that we'll expose to HomeKit
+var FAKE_SENSOR = {
+  motionDetected: false,
+  getMotion: function() { 
+    return FAKE_SENSOR.motionDetected;
+  },
+  randomize: function() {
+    // randomize temperature to a value between 0 and 100
+    FAKE_SENSOR.motionDetected = Math.round(Math.random());
+  }
+}
+
+
+// Generate a consistent UUID for our Temperature Sensor Accessory that will remain the same
+// even when restarting our server. We use the `uuid.generate` helper function to create
+// a deterministic UUID based on an arbitrary "namespace" and the string "temperature-sensor".
+var sensorUUID = uuid.generate('hap-nodejs:accessories:motion-sensor');
+
+// This is the Accessory that we'll return to HAP-NodeJS that represents our fake lock.
+var sensor = exports.accessory = new Accessory('Motion Sensor', sensorUUID);
+
+// Add the actual TemperatureSensor Service.
+// We can see the complete list of Services and Characteristics in `lib/gen/HomeKitTypes.js`
+sensor
+  .addService(Service.MotionSensor)
+  .getCharacteristic(Characteristic.MotionDetected)
+  .on('get', function(callback) {
+    
+    // return our current value
+    callback(null, FAKE_SENSOR.getMotion());
+  });
+
+// randomize our temperature reading every 3 seconds
+setInterval(function() {
+  
+  FAKE_SENSOR.randomize();
+  
+  // update the characteristic value so interested iOS devices can get notified
+  sensor
+    .getService(Service.MotionSensor)
+    .setCharacteristic(Characteristic.MotionDetected, FAKE_SENSOR.motionDetected);
+  
+}, 3000);
