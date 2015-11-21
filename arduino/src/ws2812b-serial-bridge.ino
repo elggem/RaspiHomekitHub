72b73e8b18fef876e8515d85b67aa7eb8c272d9d
Ralf Mayet <ralf@allthirds.com>
2015-11-21 17:11:29 +0100

    arduino

--- arduino/src/ws2812b-serial-bridge.ino
+++ arduino/src/ws2812b-serial-bridge.ino
@@ -32,6 +32,7 @@ void setup() {
   }
 
   //init strips:
+  strip.begin();
   strip_a.begin();
   strip_b.begin();
   
@@ -51,8 +52,7 @@ void setup() {
 
 void loop() {
 
-rainbow(strip_a, 50);
-rainbow(strip_b, 50);
+theaterChaseRainbow(50);
 
   if (Serial.available() > 0) {
     // get incoming byte:
@@ -141,7 +141,23 @@ uint32_t Wheel(byte WheelPos) {
 
 
 
+//Theatre-style crawling lights with rainbow effect
+void theaterChaseRainbow(uint8_t wait) {
+  for (int j=0; j < 256; j++) {     // cycle all 256 colors in the wheel
+    for (int q=0; q < 3; q++) {
+      for (int i=0; i < strip.numPixels(); i=i+3) {
+        strip.setPixelColor(i+q, Wheel( (i+j) % 255));    //turn every third pixel on
+      }
+      strip.show();
 
+      delay(wait);
+
+      for (int i=0; i < strip.numPixels(); i=i+3) {
+        strip.setPixelColor(i+q, 0);        //turn every third pixel off
+      }
+    }
+  }
+}
 
 
 
