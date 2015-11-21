824bee4c78124d1e74c6d37348adca781a987a6c
Ralf Mayet <ralf@raspberrypi>
2015-11-21 23:57:19 +0000

    port change

--- accessories/arduino_accessory.js
+++ accessories/arduino_accessory.js
@@ -4,7 +4,7 @@ var Characteristic = require('../').Characteristic;
 var uuid = require('../').uuid;
 
 var SerialPort = require("serialport").SerialPort
-var serialPort = new SerialPort("/dev/ttyUSB1", { baudrate: 9600 });
+var serialPort = new SerialPort("/dev/ttyUSB0", { baudrate: 9600 });
 var connectionActive = false;
 
 serialPort.on("open", function () {
