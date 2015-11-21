1a54d40e2c52491a2a0c7866fe54d1253b91033c
Ralf Mayet <ralf@allthirds.com>
2015-11-21 16:51:53 +0100

    arduino

--- arduino/src/ws2812b-serial-bridge.ino
+++ arduino/src/ws2812b-serial-bridge.ino
@@ -50,6 +50,8 @@ void setup() {
 
 void loop() {
 
+rainbow(strip_a, 50)
+rainbow(strip_b, 50)
 
   if (Serial.available() > 0) {
     // get incoming byte:
@@ -106,14 +108,14 @@ String getValue(String data, int index)
 
   return found>index ? data.substring(strIndex[0], strIndex[1]) : "";
 }
-/*
 
-void rainbow(uint8_t wait) {
+
+void rainbow(Adafruit_NeoPixel strip, uint8_t wait) {
   uint16_t i, j;
 
   for(j=0; j<256; j++) {
     for(i=0; i<strip.numPixels(); i++) {
-      strip.setPixelColor(i, Wheel((i+j) & 255));
+      strip.setPixelColor(i, Wheel(strip, (i+j) & 255));
     }
     strip.show();
     delay(wait);
@@ -123,7 +125,7 @@ void rainbow(uint8_t wait) {
 
 // Input a value 0 to 255 to get a color value.
 // The colours are a transition r - g - b - back to r.
-uint32_t Wheel(byte WheelPos) {
+uint32_t Wheel(Adafruit_NeoPixel strip, byte WheelPos) {
   WheelPos = 255 - WheelPos;
   if(WheelPos < 85) {
     return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
@@ -135,7 +137,7 @@ uint32_t Wheel(byte WheelPos) {
   WheelPos -= 170;
   return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
 }
-*/
+
 
 
 
